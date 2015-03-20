dc5dd6d1962c8eda4bce64c2d4e4a2451746cdfd
Vladimirs Ivanovs <ivanovs223@gmail.com>
2015-03-20 11:49:47 +0000

    Updated test cases.

--- src/tests/ContactImplTest.java
+++ src/tests/ContactImplTest.java
@@ -1,37 +1,56 @@
 package tests;
 
 import static org.junit.Assert.*;
+import interfaces.Contact;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class ContactImplTest {
+	Contact person1;
+	Contact person2;
+	Contact person3;
+	String note1;
+	String note2;
 	
 	@Before
 	public void initialize(){
-		Contact contact = new ContactImpl(name, notes);
+		String name = "Jhon Jhonson"; 	//name of the person
+		note1 = "";						//empty string comment
+		note2 = "Test note";			//normal comment
+		person1 = new ContactImpl(name, note1);
+		person2 = new ContactImpl(name, note2);
+		person3 = new ContactImpl(name, null);	//comment is set to Null
 	}
 
+	/**
+	 * Tests default behavior of the method.
+	 */
 	@Test
-	public void testAddNotesDefault() {
-		String note = "This is a note about this contact.";
-		contact.addNotes(note);
-		assertEquals(cont. contact.getNotes());
+	public void testGetNotesDefault() {
+		assertEquals(note1, person1.getNotes());
+		assertEquals(note2, person2.getNotes());
 	}
 	
+	/**
+	 * Tests if method returns empty string if notes are set to null.
+	 */
 	@Test
-	public void testAddNotesEmptyString() {
-		String note = "";
-		contact.addNotes(note);
-		assertEquals(cont.)
-		assertEquals(note, contact.getNotes());
+	public void testGetNotesNull() {
+		assertNotNull(person3.getNotes());
+		assertEquals(note1, person3.getNotes());
 	}
 	
+	/**
+	 * Tests if IDs of contacts are set correctly.
+	 * Unique ID of the contact equals to a total count number value on the moment
+	 * of creating it.
+	 */
 	@Test
-	public void testAddNotesNull() {
-		String note = null;
-		contact.addNotes(note);
-		assertNull(contact.getNotes());
+	public void testGetId() {
+		assertEquals(1, person1.getId());
+		assertEquals(2, person1.getId());
+		assertEquals(3, person3.getId());
+		
 	}
-
 }
